5758990c1028532dd7873879570474e38e043984
Renan Campos <renanc433@gmail.com>
2021-03-04 22:51:24 -0300

    operation arguments

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import {GraphQLServer} from 'graphql-yoga'
 //type definitions (schema)
 const typeDefs = `
   type Query {
+   greeting(name: String, position: String): String!
+   sum(firstNumber: Int!, secondNumber: Int!): Int!
    me: User!
    post: Post!
   }
@@ -25,6 +27,17 @@ const typeDefs = `
 //resolvers
 const resolvers = {
   Query: {
+    sum(parent, args, ctx, info){
+      if(args.firstNumber && args. secondNumber) {
+        return args.firstNumber + args. secondNumber
+      }
+      return 0
+    },
+    greeting(parent, args, ctx, info) {
+      console.log(args)
+      if(args.name && args.position){return `Hello ${args.name} voce é ${args.position}`}
+      return 'name'
+    },
     me(){
       return {
         id: '123',
