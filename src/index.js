6af7b1d4acc3ceb0c8f9a05aa39930d174980cd4
Renan Campos <renanc433@gmail.com>
2021-03-06 20:05:20 -0300

    filtering posts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import {GraphQLServer} from 'graphql-yoga'
 //demo data
-
 const users = [{
   id:'1',
   name: 'Renan',
@@ -20,10 +19,26 @@ const users = [{
   age: 1
 }
 ]
+
+const posts = [{
+  id:'1',
+  author: 'Renan Campos',
+  title: 'Batatinha',
+  body: 'Batatinha quando nasce se esparrama pelo chao'
+},
+{
+  id:'2',
+  author: 'Hernandes Campos',
+  title: 'The it',
+  body: 'The it is the thing that is not the thing'
+}
+]
+
 //type definitions (schema)
 const typeDefs = `
   type Query {
     users(query: String):[User!]!
+    posts(query: String):[Post!]!
     me: User!
     post: Post!
   }
@@ -54,6 +69,16 @@ const resolvers = {
       return users.filter(user=>{
         return user.name.toLocaleLowerCase().includes(args.query.toLocaleLowerCase())
       })
+    },
+    posts(parent, args, ctx, info){
+      if(!args.query){
+        return posts
+      }
+
+      return posts.filter(post => {
+        return post.title.toLocaleLowerCase().includes(args.query.toLocaleLowerCase())
+      })
+
     },
     me(){
       return {
